9e7237510c1f692f054c16aeb23dd258b2a8904b
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-12-17 18:48:36 +0100

    Add module P0102_ActivityListener

--- app/src/main/java/ua/com/sevenpowerx/android/p0101_p0111_listener/MainActivity.java
+++ app/src/main/java/ua/com/sevenpowerx/android/p0101_p0111_listener/MainActivity.java
@@ -2,12 +2,47 @@ package ua.com.sevenpowerx.android.p0101_p0111_listener;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import static android.view.View.*;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tvOut;
+    Button btnOk;
+    Button btnCancel;
 
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // найдем View-элементы
+        tvOut = (TextView) findViewById(R.id.tvOut);
+        btnOk = (Button) findViewById(R.id.btnOk);
+        btnCancel = (Button) findViewById(R.id.btnCancel);
+
+        // создание обработчика
+        OnClickListener oclBtn = new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // по id определеяем кнопку, вызвавшую этот обработчик
+                switch (v.getId()) {
+                    case R.id.btnOk:
+                        // кнопка ОК
+                        tvOut.setText("Нажата кнопка ОК");
+                        break;
+                    case R.id.btnCancel:
+                        // кнопка Cancel
+                        tvOut.setText("Нажата кнопка Cancel");
+                        break;
+                }
+
+            }
+        };
+        btnOk.setOnClickListener(oclBtn);
+        btnCancel.setOnClickListener(oclBtn);
     }
 }
